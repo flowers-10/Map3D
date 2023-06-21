7f263f37ee9a54c204e7f88719d342b7c3d443ad
Michelangelo Lv <mgaligigi@163.com>
2023-06-21 23:23:58 +0800

    feat:随机点位完整版

--- src/views/Point/index.vue
+++ src/views/Point/index.vue
@@ -3,13 +3,46 @@
 </template>
 
 <script lang="ts" setup>
-import HangZhou from "../../assets/JSON/HangZhou.json";
-import points from "./generateUniformPoints";
 // 引入工具
 import * as echarts from "echarts";
 import "echarts-gl"; //3D地图插件
 import { onMounted } from "vue";
+import * as turf from "@turf/turf";
+import HangZhou from "../../assets/JSON/HangZhou.json";
 
+// 杭州的边界范围
+const hzBoundaries: turf.Position[] =
+  HangZhou.features[0].geometry.coordinates[0][0];
+
+function generateUniformPoints(
+  count: number,
+  boundaries: turf.Feature<turf.Polygon>
+): turf.Position[] {
+  const points: turf.Position[] = [];
+  const bbox: turf.BBox = turf.bbox(boundaries);
+  const cellSizeX: number = (bbox[2] - bbox[0]) / Math.ceil(Math.sqrt(count));
+  const cellSizeY: number = (bbox[3] - bbox[1]) / Math.ceil(Math.sqrt(count));
+
+  for (let x = bbox[0] + cellSizeX / 2; x < bbox[2]; x += cellSizeX) {
+    for (let y = bbox[1] + cellSizeY / 2; y < bbox[3]; y += cellSizeY) {
+      const pt: turf.Feature<turf.Point> = turf.point([x, y]);
+      if (turf.booleanPointInPolygon(pt, boundaries)) {
+        points.push([x, y]);
+      }
+      if (points.length >= count) {
+        break;
+      }
+    }
+    if (points.length >= count) {
+      break;
+    }
+  }
+
+  return points;
+}
+
+const boundaries: turf.Feature<turf.Polygon> = turf.polygon([hzBoundaries]);
+const points: turf.Position[] = generateUniformPoints(10000, boundaries);
 const DataBit: any = [];
 for (let item of points) {
   item[2] = Math.random() * 40;
@@ -21,77 +54,8 @@ console.log(DataBit);
 // 定义echarts方法
 const chartMap = () => {
   var myChart = echarts.init(document.getElementById("mapEchart")!);
-  // 重点：不要遗漏这句代码！！
   echarts.registerMap("zhejiang", <any>HangZhou);
   // 图表配置项
-  // map3D
-  // var option = {
-
-  //   visualMap: {
-  //     show: false,
-  //     calculable: true,
-  //     realtime: false,
-  //     dimension: 3,
-  //     inRange: {
-  //       color: [
-  //         "#313695",
-  //         "#4575b4",
-  //         "#74add1",
-  //         "#abd9e9",
-  //         "#e0f3f8",
-  //         "#ffffbf",
-  //         "#fee090",
-  //         "#fdae61",
-  //         "#f46d43",
-  //         "#d73027",
-  //         "#a50026",
-  //       ],
-  //     },
-  //     outOfRange: {
-  //       colorAlpha: 0,
-  //     },
-  //   },
-
-  //   mapbox3D: {
-  //     center: [121.4293, 31.20307],
-  //     zoom: 11,
-  //     pitch: 50,
-  //     bearing: 30,
-  //     style: "mapbox://styles/mapbox/light-v9",
-  //     // altitudeScale: 1,
-  //     postEffect: {
-  //       enable: true,
-  //       FXAA: {
-  //         enable: true,
-  //       },
-  //     },
-  //     light: {
-  //       main: {
-  //         intensity: 1,
-  //         shadow: true,
-  //         shadowQuality: "high",
-  //       },
-  //       ambient: {
-  //         intensity: 0,
-  //       },
-  //       ambientCubemap: {
-  //         texture: "/asset/get/s/data-1491838644249-ry33I7YTe.hdr",
-  //         exposure: 1,
-  //         diffuseIntensity: 0.5,
-  //         specularIntensity: 2,
-  //       },
-  //     },
-  //   },
-  //   series: [
-  //     {
-  //       type: "bar3D",
-  //       shading: "realistic",
-  //       coordinateSystem: "mapbox3D",
-  //       barSize: 0.4,
-  //       silent: true,
-  //     },
-  //   ],
-  // };
   // Geo3D
   let option = {
     backgroundColor: "#cdcfd5",
@@ -193,7 +157,7 @@ const chartMap = () => {
       },
     ],
   };
-  
+
   myChart.setOption(option);
 };
 
