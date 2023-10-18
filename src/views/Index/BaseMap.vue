7651153b46365931643638aa34ce7b40e8dcb710
Michela <mgaligigi@163.com>
2023-10-18 16:20:05 +0800

    refactor:更新BaseMap代码(【3d地图】vue中使用echarts geo3D)

--- src/views/Index/BaseMap.vue
+++ src/views/Index/BaseMap.vue
@@ -12,126 +12,70 @@ import "echarts-gl"; //3D地图插件
 
 export default {
   methods: {
-    // 2D地图贴图的配置项
-    get2DMapOption() {
-      const chartOption = {
-        tooltip: {
-          show: true,
-        },
-        geo: {
-          show: true,
-          map: "ok", // 重要！！！注册的地图名字叫啥，这里就填啥
-          //  重要！！一定要配置好宽高，否则导致地图和3D地图不重合
-          left: 0,
-          top: 0,
-          right: 0,
-          bottom: 0,
-          width: 920,
-        },
-        series: [
-          {
-            zlevel: 100,
-            name: "散点",
-            type: "effectScatter",
-            coordinateSystem: "geo",
-            symbolSize: 30,
-            tooltip: {
-              show: true,
-              formatter: (e: any) => {
-                console.log(e);
-
-                return ` ${e.name}`;
-              },
-            },
-            // 随便写点模拟数据
-            data: [
-              { name: "标签1", value: [116.554322, 39.934412, -1] },
-              { name: "标签2", value: [97.738486, 37.403523, 1] },
-              { name: "标签3", value: [109.586346, 25.217832, 2] },
-              { name: "标签4", value: [118.417048, 33.33643, 3] },
-              { name: "标签5", value: [118.417048, 33.33643, 4] },
-              { name: "标签6", value: [132.546172, 46.523096, 5] },
-              { name: "标签7", value: [83.977308, 30.703477, 6] },
-            ],
-          },
-        ],
-      };
-
-      return chartOption;
-    },
-
-    Load3DMap(chartDOM: any) {
-      // 先渲染2D地图作为贴图
-      const canvas = document.createElement(`canvas`);
-      // 重要！！
-      var mapBg = echarts.init(canvas, undefined, {
-        // 重要！！一定要配置好宽高，否则导致地图和3D地图不重合
-        width: 1024,
-        height: 1024,
-      });
-      // 获取2D地图的配置项
-      const chartOption = this.get2DMapOption();
-      // 2D的地图DOM
-      mapBg.setOption(chartOption);
-      // 3D地图配置项
-      const option = {
-        tooltip: {
-          show: true,
-        },
-        geo3D: {
-          zlevel: 1,
-          map: "ok", // 重要！！！注册的地图名字叫啥，这里就填啥
-          shading: "color", // 重要！！！选择texture渲染的方式
-          colorMaterial: {
-            detailTexture: mapBg, //重要！！！2D地图的DOM作为纹理贴图放到3D上
-            textureTiling: 1, // 纹理平铺，1是拉伸，数字表示纹理平铺次数
-          },
-          itemStyle: {
-            color: "#ccc",
-            opacity: 1,
-            borderWidth: 1,
-            borderColor: "#96ebf7",
-          },
-        },
-        series: [
-          {
-            zlevel: 100,
-            type: "bar3D",
-            coordinateSystem: "geo3D",
-            barSize: 1,
-            tooltip: {
-              show: true,
-              formatter: (e: any) => {
-                console.log(e);
-
-                return ` ${e.name}`;
-              },
-            },
-            // 随便写点模拟数据
-            data: [
-              { name: "标签1", value: [116.554322, 39.934412, 10] },
-              { name: "标签2", value: [97.738486, 37.403523, 1] },
-              { name: "标签3", value: [109.586346, 25.217832, 2] },
-              { name: "标签4", value: [118.417048, 33.33643, 3] },
-              { name: "标签5", value: [118.417048, 33.33643, 4] },
-              { name: "标签6", value: [132.546172, 46.523096, 5] },
-              { name: "标签7", value: [83.977308, 30.703477, 6] },
-            ],
-          },
-        ],
-      };
-      // // 渲染3D地图（已经把2D地图作为材质贴到3D地图上了）
-      chartDOM.setOption(option, true);
-    },
     chartMap() {
-      const myChart = echarts.init(
-        document.getElementById("mapEchart") as HTMLElement
-      );
-      echarts.registerMap("ok", geoJson);
-      this.Load3DMap(myChart);
-
-     
-    },
+     var myChart = echarts.init(document.getElementById("mapEchart") as HTMLElement);
+      echarts.registerMap("zhejiang", geoJson as any);
+      myChart.hideLoading();
+      // 图表配置项
+      let option = {
+        geo3D: {
+          map: "zhejiang",
+          roam: true,
+          itemStyle: {
+            color: "#007aff",
+            opacity: 0.8,
+            borderWidth: 0.4,
+            borderColor: "#000"
+          },
+          viewControl: {
+            autoRotate: true,
+            autoRotateAfterStill: 3,
+            distance: 120,
+            minAlpha: 5, // 上下旋转的最小 alpha 值。即视角能旋转到达最上面的角度。[ default: 5 ]
+            maxAlpha: 90, // 上下旋转的最大 alpha 值。即视角能旋转到达最下面的角度。[ default: 90 ]
+            minBeta: -360, // 左右旋转的最小 beta 值。即视角能旋转到达最左的角度。[ default: -80 ]
+            maxBeta: 360, // 左右旋转的最大 beta 值。即视角能旋转到达最右的角度。[ default: 80 ]
+            animation: true, // 是否开启动画。[ default: true ]
+            animationDurationUpdate: 1000, // 过渡动画的时长。[ default: 1000 ]
+            animationEasingUpdate: "cubicInOut" // 过渡动画的缓动效果。[ default: cubicInOut ]
+          },
+          emphasis: {
+            disabled: true, //是否可以被选中
+            label: {
+              //移入时的高亮文本
+              show: true,
+              color: "#333", //显示字体颜色变淡
+              fontSize: 18 //显示字体变大
+            },
+            itemStyle: {
+              color: "#ff7aff" //显示移入的区块变粉色
+            }
+          },
+          label: {
+            show: true,
+            position: "top",
+            color: "#111", //地图初始化区域字体颜色
+            fontSize: 14,
+            lineHeight: 16
+          },
+          shading: "lambert",
+          light: {
+            //光照阴影
+            main: {
+              intensity: 1, //光照强度
+              shadow: true, //是否显示阴影
+              shadowQuality: "medium", //阴影质量 ultra //阴影亮度
+              alpha: 55,
+              beta: 10
+            },
+            ambient: {
+              intensity: 0.7
+            }
+          }
+        },
+      };
+      myChart.setOption(option);
+    }
   },
   mounted() {
     this.chartMap();
