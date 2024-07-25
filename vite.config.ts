5fdd914d63971e7c896d9db8d0b6897139e3eb6b
Michela <mgaligigi@163.com>
2024-07-25 15:26:30 +0800

    chore:add vercel config

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "./",
+  base: "",
   publicDir: "./public",
   assetsInclude: ["**/*.glb", "**/*.hdr", "**/*.mp3", "**/*.ico","**/*.svg"],
   plugins: [vue()],
