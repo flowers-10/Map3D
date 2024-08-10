fdc9c638a396adfc702024da5c8e1aede5604e40
Michela <mgaligigi@163.com>
2024-08-10 23:13:37 +0800

    release:v3.0.0

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,3 +1,7 @@
 /// <reference types="vite/client" />
-declare module 'd3';
-declare module 'd3-geo';
+declare module "d3";
+declare module "d3-geo";
+declare module "*.glsl" {
+  const shader: string;
+  export default shader;
+}
